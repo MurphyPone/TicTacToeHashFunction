aebec4651698bb7ab500b67fbeafcbe4074c6c27
"[MurphyPOne]" <"[murphyp1@bishopireton.org]">
2018-03-21 22:37:38 -0400

    differentiate between invalid and lose boards

--- TicTacToeHashFunction/src/TicTacToeHashCode.java
+++ TicTacToeHashFunction/src/TicTacToeHashCode.java
@@ -1,16 +1,18 @@
+import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 //TODO Make sure you remove all of the TODO comments from this file before turning itin
+
 public class TicTacToeHashCode extends Board {
 
-	boolean [] winners;  // True if the hash string that maps to this index is a winner, false otherwise
+	boolean[] winners; // True if the hash string that maps to this index is a winner, false otherwise
  	static final int[] powsOf3 = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683}; //for quick referencing
 
-	TicTacToeHashCode(String s) {
- 		super(s);	//TODO ERROR --> SUPER CALLS myHashCode...
- 		winners = new boolean[19638]; //BUT THIS ARRAY ISn't INITIALIZED YET...
+ 	TicTacToeHashCode(String s) {
+		super(s);	
+ 		winners = new boolean[(int) Math.pow(3, 9)]; 
  		//Read in file TicTacToeWinners.txt
  		Scanner input = openFile("TicTacToeWinners.txt");
  		while(input.hasNextLine()) { //while theres winners in the file
@@ -18,17 +20,13 @@ public class TicTacToeHashCode extends Board {
  			winners[myHashCode()] = true;  //set each winner[index] from the file to true
  			System.out.println( myHashCode() );
  		}
-  }
-  
- 	// TODO - write the myHashCode function.  It must create a unique hashcode for all of the 
- 	//   possible values the game board (3 ^ 9) and it MUST use the super.charAt(row, col) function
- 	@Override
- 	public int myHashCode() {
- 		int sum = 0;	//invalid by default
+	}
+
+	@Override
+	public int myHashCode() {
+		int sum = 0;	//invalid by default
  		int move = -1;	//hash value of the current character
 
-		
- 		//if( (board.length()) == LENGTH ) {	//make sure the board is valid
  		for(int r = 0; r < TicTacToe.ROWS; r++) {	//iterate through the rows 
  			for(int c = 0; c < TicTacToe.COLS; c++) {	//iterate through the cols 
  				char current = (char) charAt(r, c);		//Get hashCode of charAt(r, c)
@@ -41,10 +39,9 @@ public class TicTacToeHashCode extends Board {
 			}
 		}
 		return sum; 
-		// else invalid board return sum;
-   }
-  
- 	// MY ADDED HELPER METHOD //
+	}
+	
+	// MY ADDED HELPER METHOD //
  	static int charCode(char x) {
  		//Use single quotes to indicate it's a char not a String
  		if( x == ' ') return 0;
@@ -66,23 +63,26 @@ public class TicTacToeHashCode extends Board {
 		}
 		return input;
 	}
-  
-   public static void main(String[] args) throws InterruptedException {
-      TicTacToeHashCode board = new TicTacToeHashCode ("Tic Tac Toe");
-      while (true) {
-    	  board.setBoardString("xxox xoo "); //TODO IS THIS ALLOWED?
-         
-    	  board.displayRandomString();
-    	  board.setHashCodeLabel( board.myHashCode() );  //change the label to the current 
-    	  board.setWinnerLabel( board.isWin() );
-         
-    	  Thread.sleep(10000); //4,000      
-      }
-   }
 
 	@Override
-	boolean isWin() {
+	public boolean isWin(String s) {
+      // TODO write an isWin method that takes in a String.  This should not change the board.  Board has an additional charAt 
+      // TODO method to facilitate this
+		return false;
+      }
+      
+	@Override
+	public boolean isWin() {
+		System.out.println(winners[myHashCode()]);
 		return winners[myHashCode()];
+      }
+      
+	public static void main(String[] args) throws InterruptedException {
+		TicTacToeHashCode board = new TicTacToeHashCode("Tic Tac Toe");
+		 /* while (true) {
+		   board.displayRandomString();
+		   Thread.sleep(10000); //4 seconds usually 
+		 } */
 	}
-}  
-  
+
+}
